7e171efa0ed93e0c0ac8ca78eda28bc8f7cdeffe
Natalie Chufistova <nataliech0409@gmail.com>
2026-01-20 10:05:27 +0300

    view component

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>hello app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum" }} >
+      <View style={{ width: "100%", height: 50, backgroundColor: "powderblue" }}></View>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
